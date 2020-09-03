7948121558c4ada737efb7be8d6d0c5f79db0938
imdreamer <743295483@qq.com>
2020-09-03 17:56:06 +0800

    [yangqian] fix: flatten array function in exercise_05.

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -2,9 +2,11 @@ export default function flattenArray(array) {
   // This function flattens a nested array into a sequence.
   //
   // Your target:
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  return array.flat();
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
 }
