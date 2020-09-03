0f0210cab5addbda81755e4fd2a48bf775913047
imdreamer <743295483@qq.com>
2020-09-03 20:03:09 +0800

    [yangqian] fix: vector about how to defined class's property and function etc. in exercise_06.

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -7,6 +7,41 @@ export default class Vector {
   // * We should be able to calculate the distance of a vector.
   //
   // Your target:
+  constructor(x, y) {
+    this.__getX__ = () => x;
+    this.__getY__ = () => y;
+  }
+
+  get x() {
+    return this.__getX__();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  set x(val) {
+    throw new Error('The should not set x and y of the vector');
+  }
+
+  get y() {
+    return this.__getY__();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  set y(val) {
+    throw new Error('The should not set x and y of the vector');
+  }
+
+  static plus(vector1, vector2) {
+    return new Vector(vector1.x + vector2.x, vector1.y + vector2.y);
+  }
+
+  static minus(vector1, vector2) {
+    return new Vector(vector1.x - vector2.x, vector1.y - vector2.y);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  distance() {
+    return Math.hypot(this.x, this.y);
+  }
   //
   // * Please implement the class and pass all the tests in vector_spec.js.
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
