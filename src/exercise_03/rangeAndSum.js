b5b53030e35fc7b614471089434d70d599e3c19a
imdreamer <743295483@qq.com>
2020-09-03 17:38:00 +0800

    [yangqian] fix: range and sum function in exercise_03.

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -3,20 +3,29 @@ export function range(start, end) {
   // and `end`.
   //
   // Your target:
+  const result = [];
+  if (start > end) {
+    for (let i = start; i > end; i -= 1) {
+      result.push(i);
+    }
+  } else if (start < end) {
+    for (let i = start; i < end; i += 1) {
+      result.push(i);
+    }
+  }
+  return result;
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
 }
 
 export function sum(...numbers) {
   // This function sums all the numbers in the array and returns the final result.
   //
   // Your target:
+  if (numbers === undefined || numbers === null || numbers.length === 0) return 0;
+  return numbers.reduce((previousValue, currentValue) => previousValue + currentValue);
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
 }
