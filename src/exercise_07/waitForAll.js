ee9a87e9f7506fa0084f95faee8523bfb54f0d2e
imdreamer <743295483@qq.com>
2020-09-04 11:11:32 +0800

    [yangqian] fix: wait for all in exercise_07.

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -5,9 +5,31 @@ export default function waitForAll(...promises) {
   // if one of the `promises` is rejected, then the returned promise will be rejected.
   //
   // Your target:
+  promises.forEach((promise) => {
+    if (!(promise instanceof Promise)) {
+      throw Error('Not all elements are promises.');
+    }
+  });
+  return new Promise(((resolve, reject) => {
+    let counter = 0;
+    for (let i = 0; i < promises.length; i += 1) {
+      // eslint-disable-next-line no-loop-func
+      promises[i].then(() => {
+        counter += 1;
+        if (counter === promises.length) {
+          resolve();
+        }
+      }, () => {
+        'It should not be a success promise';
+        // eslint-disable-next-line no-loop-func
+      }).finally(() => {
+        if (i === promises.length - 1 && counter !== promises.length) {
+          reject();
+        }
+      });
+    }
+  }));
   //
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
 }
